e9879e19cc74389e7db44eef4111ed3b023a202c
LeoHolmer <leonardoholmer1@gmail.com>
2025-12-17 00:37:50 -0300

    Completed JPA repositories

--- src/main/java/com/taskflow/api/repository/TaskRepository.java
+++ src/main/java/com/taskflow/api/repository/TaskRepository.java
@@ -1,7 +1,16 @@
 package com.taskflow.api.repository;
 
 import com.taskflow.api.model.Task;
+import com.taskflow.api.model.enums.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByStatus(TaskStatus status);
+
+    List<Task> findByAssignedUserId(Long userId);
+
+    List<Task> findByProjectId(Long projectId);
 }
--- src/main/java/com/taskflow/api/repository/UserRepository.java
+++ src/main/java/com/taskflow/api/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.taskflow.api.repository;
 import com.taskflow.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User , Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
